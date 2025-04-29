72823c56898534ca237afad388a3ba5aca9646e4
ElizavetaKr <elizovetakryukova@mail.ru>
2025-04-29 18:33:08 +0300

    4. Микросервисные системы [#338434]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,12 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.ProfileDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -23,6 +26,7 @@ public class IndexController {
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -41,7 +45,14 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
+        List<InterviewDTO> interviews = interviewsService.getByType(1);
+        model.addAttribute("new_interviews", interviews);
+        List<ProfileDTO> authors = new ArrayList<>();
+        for (InterviewDTO i : interviews) {
+            Optional<ProfileDTO> profileDTO = profilesService.getProfileById(i.getSubmitterId());
+            authors.add(profileDTO.orElse(null));
+        }
+        model.addAttribute("authors", authors);
         return "index";
     }
 }
